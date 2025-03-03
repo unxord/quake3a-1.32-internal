fc4c56ecc93e3db641d7bfa6e3b4e7e85f43bb91
unxord <unxord@gmail.com>
2025-03-04 02:13:36 +0500

    Создание базовой DLL и инъекция

---
+++ quake3a132/dllmain.cpp
@@ -0,0 +1,15 @@
+﻿#include <windows.h>
+
+// Точка входа для DLL
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        // Выполняется при загрузке DLL в процесс
+        MessageBox(NULL, L"DLL Successfully Injected into Quake 3 Arena!", L"Success", MB_OK | MB_ICONINFORMATION);
+        break;
+    case DLL_PROCESS_DETACH:
+        // Выполняется при выгрузке DLL
+        break;
+    }
+    return TRUE;
+}
